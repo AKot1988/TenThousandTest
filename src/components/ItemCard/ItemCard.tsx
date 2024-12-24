fa3b2694cd3fecd0189b5e8c12ae966bf0e1a188
Kulibabenko Igorevich <kulibabenko@ukr.net>
2024-12-24 15:31:01 +0200

    add quantity input feat

--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addItem, removeItem } from '../../storage/itemSlice.jsx';
 import { ItemCardProps } from './helper';
@@ -11,6 +11,26 @@ const ItemCard: FC<ItemCardProps> = ({ type, onClick, ...itemData }) => {
     state.items.find((item: any) => item.id === itemData.id)
   );
   const itemQuantity = itemInCart ? itemInCart.quantity : 0;
+  const [inputQuantity, setInputQuantity] = useState(itemQuantity);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = Math.max(0, parseInt(event.target.value) || 0); // Заборона від'ємних значень
+    setInputQuantity(value);
+  };
+
+  const handleUpdateQuantity = () => {
+    const difference = inputQuantity - itemQuantity;
+
+    if (difference > 0) {
+      for (let i = 0; i < difference; i++) {
+        dispatch(addItem(itemData));
+      }
+    } else if (difference < 0) {
+      for (let i = 0; i < Math.abs(difference); i++) {
+        dispatch(removeItem(itemData));
+      }
+    }
+  };
 
   const handleAddAction = () => {
     dispatch(addItem(itemData));
@@ -38,7 +58,18 @@ const ItemCard: FC<ItemCardProps> = ({ type, onClick, ...itemData }) => {
           <p>{itemData.manufacturer}</p>
           <p>{`price ${itemData.price} $`}</p>
           <p>{`in cart: ${itemQuantity}`}</p>
-          <button onClick={handleAddAction}>Add to Cart</button>
+          <div className={classes.quantityControl}>
+            <label htmlFor={`quantity-${itemData.id}`}>Кількість:</label>
+            <input
+             className={classes.quantityInput}
+              id={`quantity-${itemData.id}`}
+              type="number"
+              value={inputQuantity}
+              min="0"
+              onChange={handleInputChange}
+            />
+            </div>
+          <button onClick={handleUpdateQuantity}>Add to Cart</button>
           <button disabled={itemQuantity === 0} onClick={handleRemoveAction}>
             Remove from Cart
           </button>
